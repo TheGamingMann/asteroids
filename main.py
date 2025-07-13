3b0f5c57c1e30b03b6e6571593f2dab4ea414b7c
TheGamingMann <dylan.mann43@gmail.com>
2025-07-13 16:29:51 -0400

    Added asteroid spliting and shooting!

--- main.py
+++ main.py
@@ -13,9 +13,11 @@ def main():
     updateable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     Player.containers = (updateable, drawable)
     Asteroid.containers = (updateable, drawable, asteroids)
     AsteroidField.containers = (updateable)
+    Shot.containers = (updateable, drawable, shots)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     asteroid_field = AsteroidField()
     while True:
@@ -30,6 +32,12 @@ def main():
             if asteroid.collision(player):
                 print("Game over!")
                 sys.exit()
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collision(shot):
+                    asteroid.split()
+                    shot.kill()
+                
             
             
         pygame.display.flip()
