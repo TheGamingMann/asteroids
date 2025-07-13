4b33f9f000801d2cca5eafa1de90d0e759d91f54
TheGamingMann <dylan.mann43@gmail.com>
2025-07-12 22:50:40 -0400

    Added asteroids with collision to player

--- main.py
+++ main.py
@@ -1,29 +1,38 @@
 import pygame
+import sys
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+    Asteroid.containers = (updateable, drawable, asteroids)
+    AsteroidField.containers = (updateable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    asteroid_field = AsteroidField()
     while True:
-        #Allow the player to quit
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        #Fill the screen black
         screen.fill((0, 0, 0))
-
-        #Draw player
-        player.draw(screen)
-
-        #Update screen
+        for item in drawable:
+            item.draw(screen)
+        updateable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collision(player):
+                print("Game over!")
+                sys.exit()
+            
+            
         pygame.display.flip()
-        
-        #Limit FPS to 60
         dt = clock.tick(60)/1000
 
 
